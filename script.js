96f2aad9e2187b64bff45613020911207f2f96f3
hotmailbelike <fishilike.fish@gmail.com>
2019-09-30 09:10:07 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 /* JavaScript code here */
 console.log("Police");
+
+var arr = [2,4,6,8];
+
+var sum = (a) => {
+    var x=0;
+    for(var i=0;i<a.length;i++){
+        x+=a[i];
+    }
+    return x;
+};
+
+console.log(sum(arr));
