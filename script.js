6418ed49e68d19486c876f5abffae666d4bf4c33
hotmailbelike <fishilike.fish@gmail.com>
2019-10-02 09:38:29 +0600

    Task 5

--- script.js
+++ script.js
@@ -56,3 +56,57 @@ var library = [
 
     library[1].update_status(false);
     console.log(get_status(library,1));
+
+    //Task 5:
+
+    var cart = [ 
+        {
+            name: 'Shoes',
+            price: 560,
+            quantity: 4
+        },
+        {
+            name: 'Regular Tees',
+            price: 455.50,
+            quantity: 6
+        },
+        {
+            name: 'Socks',
+            price: 65.99,
+            quantity: 2
+        }];
+
+    function add_to_cart(new_name,new_price,new_quantity){
+        cart.push({name: new_name,price: new_price, quantity: new_quantity});
+    }
+
+    function add_to_cart2(new_name,new_price,new_quantity){
+        var temp ={};
+        temp.name = new_name;
+        temp.price = new_price;
+        temp.quantity = new_quantity;
+    }
+
+    function add_to_cart3(newItem){
+        cart.push(newItem);
+    }
+
+    add_to_cart('Police',65,4);
+
+    console.log(cart);
+
+
+    function sortByKey(key){
+        cart.sort(function(a, b) {
+           return a[key] - b[key];
+        });   
+        /*
+        cart.sort((a, b) => a[price] - b[price]);
+        */
+    }
+
+    //[] when passing a value
+
+    sortByKey('quantity');
+
+    console.log(cart);
